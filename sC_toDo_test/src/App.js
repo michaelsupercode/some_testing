c105439066559771000eb307eee2922ada470ad7
Michael Hanel <michaelsupercode@gmail.com>
2023-10-26 11:36:00 +0200

    set

---
+++ sC_toDo_test/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ToDoList from './components/ToDoList';
+
+function App() {
+    return ( 
+<div className = "App" >
+        <h3> ToDo's </h3> 
+        <ToDoList / >
+        </div>
+    );
+}
+
+export default App;
