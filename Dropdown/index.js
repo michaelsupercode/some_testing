51937b42e99448e7662364cf782d99ff6b68b234
Michael Hanel <michaelsupercode@gmail.com>
2023-11-16 15:42:59 +0100

    rev

--- Dropdown/index.js
+++ Dropdown/index.js
@@ -6,7 +6,8 @@ btn.addEventListener("click", () => {
   nav.classList.toggle("active");
 
   img.src.includes("./menue.png")
-    ? (img.src = "./closed.png")
-    : (img.src = "./menue.png");
+  ? (img.src = "./menue.png")
+  : (img.src = "./closed.png")
+
 });
 
