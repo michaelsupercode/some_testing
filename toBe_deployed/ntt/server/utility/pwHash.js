3e1306dc4915a8f46cc01cce53e0328b7cbfae4a
Michael Hanel <michaelsupercode@gmail.com>
2023-12-22 12:58:28 +0100

    ends

--- toBe_deployed/ntt/server/utility/pwHash.js
+++
@@ -1,9 +0,0 @@
-const crypto = require("crypto");
-
-const hashPassword = (password) => {
-  return crypto.createHash("sha256").update(password, "utf8").digest("hex");
-};
-
-module.exports = {
-  hashPassword,
-};
