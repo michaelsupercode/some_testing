530a7b54692ff13b18e5f4c1a78a3dcef3578590
Michael Hanel <michaelsupercode@gmail.com>
2024-02-12 18:31:03 +0100

    rev

--- docker_test/app.js
+++ docker_test/app.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = "8828";
 
 app.get("/", (_, res) => {
-  res.json({ message: "My first Docker" });
+  res.json({ message: "Docker" });
 });
 
 app.listen(PORT, () => {
