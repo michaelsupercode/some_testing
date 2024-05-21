27a6527778aad13db1f936ec5f4440f48128af7b
Michael Hanel <michaelsupercode@gmail.com>
2024-05-21 07:11:48 +0200

    set

--- z_landing_mi/src/components/ButtonContact/ButtonContact.jsx
+++
@@ -1,8 +0,0 @@
-export const ButtonContact = () => {
-  // # Scroll to Contact Form
-  return (
-    <a href='mailto:michael@mhanel.de' className='btn-contact'>
-      eMail
-    </a>
-  )
-}
--- z_landing_mi/src/context/Context.jsx
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-
-export const darkModeContext = createContext();
-export const iconStyleContext = createContext();
