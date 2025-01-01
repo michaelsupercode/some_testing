6144da008fbe78f3baf28b3f6306364f6bd253bd
130sf <mdens@gmx.net>
2025-01-01 15:22:11 +0100

    clear

--- login_noLogin/server/db-access/insertions.js
+++
@@ -1,9 +0,0 @@
-const {_getDb} = require('./_getDb')
-
-async function createNewUser (user){
-    const db = await _getDb()
-    const created =  await db.collection('users').insertOne(user)
-    return created
-}
-
-module.exports = {createNewUser}
